8626aec8cdd4f766506d6a96be76511cb595f94e
jacksonroe-miami <jrr315@miami.edu>
2026-02-16 15:46:20 -0500

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,12 @@
+=begin
+
+                            Online Ruby Compiler.
+                Code, Compile, Run and Debug Ruby script online.
+Write your code in this editor and press "Run" button to execute it.
+
+=end
+
+string = "hello"
+string[0] = "H"
+
+puts string
