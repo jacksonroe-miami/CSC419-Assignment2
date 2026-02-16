194f6ef9bf04a904b29518154871268755d74477
jacksonroe-miami <jrr315@miami.edu>
2026-02-16 16:14:39 -0500

    Create outofbounds.rb

---
+++ outofbounds.rb
@@ -0,0 +1,10 @@
+=begin
+
+                            Online Ruby Compiler.
+                Code, Compile, Run and Debug Ruby script online.
+Write your code in this editor and press "Run" button to execute it.
+
+=end
+
+array = [1,2,3]
+puts array[5]
